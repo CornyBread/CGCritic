496ff0476c4797c0de33d9657260cbf6c63f6257
david <davidesber7@gmail.com>
2026-06-12 15:58:53 -0400

    Initialize the repository

---
+++ android/app/src/main/java/com/cgcritic/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cgcritic.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cgcritic.app',
+  appName: 'CGCritic',
+  webDir: 'www'
+};
+
+export default config;
